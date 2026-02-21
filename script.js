63d78cefdd8c30c7fb02f9e4a32b23f803c0f69f
Mesubir <mdsubir1999@gmail.com>
2026-02-21 20:15:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,21 @@
-// FULL PRO Clean URL System
-(function () {
+// Universal .html URL remover
+(function() {
 
-  // Remove .html from URL
-  function cleanURL() {
+  function removeHtmlFromURL() {
 
     var path = window.location.pathname;
 
     if (path.endsWith(".html")) {
 
-      var clean = path.slice(0, -5);
+      var cleanURL = path.slice(0, -5);
 
-      window.history.replaceState({}, document.title, clean);
+      window.history.replaceState(null, null, cleanURL);
 
     }
 
   }
 
-  // Handle clicks automatically
-  function handleLinks() {
-
-    document.addEventListener("click", function(e) {
-
-      var target = e.target;
-
-      if (target.tagName === "A") {
-
-        var href = target.getAttribute("href");
-
-        if (href && href.endsWith(".html")) {
-
-          e.preventDefault();
-
-          var clean = href.replace(".html", "");
-
-          window.history.pushState({}, "", clean);
-
-          window.location.href = href;
-
-        }
-
-      }
-
-    });
-
-  }
-
-  // Handle back/forward buttons
-  window.addEventListener("popstate", function() {
-
-    var path = window.location.pathname;
-
-    if (!path.endsWith(".html")) {
-
-      window.location.href = path + ".html";
-
-    }
-
-  });
-
-  // Run functions
-  window.addEventListener("load", function() {
-
-    cleanURL();
-
-    handleLinks();
-
-  });
+  // Run when page loads
+  window.addEventListener("load", removeHtmlFromURL);
 
 })();
